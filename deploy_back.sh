5ac3764228677236b8b7481e088761d666f0249d
Matias Vasconez <mavg7@outlook.com>
2025-06-20 21:02:07 -0500

    Correcciones en el deploy

--- deploy_back.sh
+++ deploy_back.sh
@@ -10,8 +10,3 @@ git pull
 # Install required Python packages
 echo "Installing dependencies..."
 pip install -r requirements.txt
-
-# Restart the backend service
-echo "Restarting services..."
-sudo systemctl restart caddy-api.service
-sudo systemctl restart caddy.service
